ba7e29311e1c6538688a13046a4e7355252739b3
zelalem888 <102322904+zelalem888@users.noreply.github.com>
2023-04-26 00:40:16 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -5,14 +5,23 @@ const minusone = document.querySelector('.a11');
 const minus = document.querySelector('.minus');
 const back = document.querySelector('.back');
 const count = document.querySelector('.count');
+const bill = document.querySelector('.bill');
+const bills = document.querySelector('.bills');
 var x = 0;
+var y = 0;
 one.addEventListener("click",() => { 
 
     x++;
+   
+    y = y + 1.6666666667;
     
     if( x >= 6){
-        alert("your free trial is over, buy full version for more clicks");
         
+        bill.innerHTML = parseInt(y);
+       x = 0;
+       return;
+        
+            
     }
     else{
         count.innerHTML = x;
@@ -21,19 +30,14 @@ one.addEventListener("click",() => {
 }
 );
 
-minus.addEventListener("click",()=>{
-    x--;
-    if(x <= -6){
-        alert("your free trial is over, buy full version for more clicks")
-    }
-    else{
-        count.innerHTML = x;
-    }
-});
+
 back.addEventListener("click",()=>{
     x = 0;
+    y = 0;
     count.innerHTML = x;
+    bill.innerHTML = y;
     var audio = new Audio("audio.mp3");
     audio.play();
 });
+
 
