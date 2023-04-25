50ccfee26c3db6bf614fe8940b02494b682d8c3d
zelalem888 <102322904+zelalem888@users.noreply.github.com>
2023-04-25 11:55:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,16 +3,16 @@ const minus = document.querySelector('.minus');
 const back = document.querySelector('.back');
 const count = document.querySelector('.count');
 var x = 0;
-add.addEventListener("click",function(){ 
+add.addEventListener("click",()=>{ 
     x++;
     count.innerHTML = x;
 }
 );
-minus.addEventListener("click",function(){
+minus.addEventListener("click",()=>{
     x--;
     count.innerHTML = x;
 });
-back.addEventListener("click",function(){
+back.addEventListener("click",()=>{
     x = 0;
     count.innerHTML = x;
 });
